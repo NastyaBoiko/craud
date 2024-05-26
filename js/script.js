fb6002e2a0710ce714435d7d184b10ce25376801
Nastya <killer99whale@mail.ru>
2024-05-26 21:11:25 +0300

    accordion js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ infoIcon.addEventListener("mouseover", function () {
 infoIcon.addEventListener("mouseout", function () {
     popup.style.opacity = 0;
 })
+
+
+const accordionBtns = document.querySelectorAll('.accordion__btn');
+
+for (let i = 0; i < accordionBtns.length; i++) {
+    accordionBtns[i].addEventListener("click", function() {
+        accordionBtns[i].classList.toggle("active");
+        let panel = this.nextElementSibling;
+        console.log(panel);
+        
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+            panel.style.paddingBottom = null;
+        } else {
+            panel.style.paddingBottom = "20px";
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+}
